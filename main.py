eb5e2666029c5312adde8dedb13307b535eee0ec
cloud <cloudintheskyoutlook.es>
2022-11-19 12:09:42 -0500

    cors fixing #11...

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
-
+#starletee
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
 #fastapi
 from fastapi import FastAPI
-from starlette.middleware.cors import CORSMiddleware
 
 #routers
 from app.routers import Integrantes,canales_twitch, tortillas
 
-
-app = FastAPI()
 #adding the CORS middleware
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
+origins=[
+    "*",
+]
 
+middleware = [
+    Middleware(CORSMiddleware, allow_origins=origins)
+]
 
+app = FastAPI(middleware=middleware)
 
 
 app.include_router(Integrantes.router)
