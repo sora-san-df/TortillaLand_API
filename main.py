6c7b05110301270948569364330d332699b68785
cloud <cloudintheskyoutlook.es>
2022-11-19 12:02:41 -0500

    cors fixing #10...

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-#python
-from typing import List
+
 #fastapi
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
@@ -10,20 +9,15 @@ from app.routers import Integrantes,canales_twitch, tortillas
 
 app = FastAPI()
 #adding the CORS middleware
-origins=[
-    "https://cq0ykg.deta.dev/integrantes",
-    "https://cq0ykg.deta.dev/integrantes/canales_twitch",
-    "https://cq0ykg.deta.dev/tortillas"
-
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["GET"],
-    allow_headers=["accept"]
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
+
 
 
 
