8b504a259fa54321d8d93bc8d1eac1b9663b5c3c
cloud <cloudintheskyoutlook.es>
2022-11-19 10:11:36 -0500

    CORSMiddleware added so in theoery now it is able to a js project request the data to the API

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
-#sys
-import sys
+#python
+from typing import List
 #fastapi
 from fastapi import FastAPI
+from fastapi.middleware.cors import  CORSMiddleware
 
 #routers
 from app.routers import Integrantes,canales_twitch, tortillas
 
 
 app = FastAPI()
+
+#accepted origins for the CORS 
+origins: list = [
+    "https://cq0ykg.deta.dev/docs",
+    "https://cq0ykg.deta.dev/integrantes",
+    "https://cq0ykg.deta.dev/canales_twitch"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=False,
+)
 
 app.include_router(Integrantes.router)
 app.include_router(canales_twitch.router)
